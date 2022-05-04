f73f75bfb5789b299172aa86355d62933add5ab2
ZakuroSoda <zakurosoda@gmail.com>
2022-05-04 13:50:00 +0800

    commit

--- popup.js
+++ popup.js
@@ -1,1 +1,7 @@
 // Future JavaScript will go here
+//Generate a random number
+function getRandom(){
+    var random = Math.random() * 100000000000000000;
+    return random;
+}
+document.getElementById("number").innerHTML = getRandom();
